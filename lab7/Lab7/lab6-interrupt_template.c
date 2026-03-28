24dbce77dd3fe5cf663e646f9c74c253742347be
Jake Kunka <bmkunka@iastat.edu>
2026-03-28 14:14:55 -0500

    lab 7

--- lab7/Lab7/lab6-interrupt_template.c
+++ lab7/Lab7/lab6-interrupt_template.c
@@ -1,13 +1,5 @@
-///**
-// * lab6-interrupt_template.c
-// *
-// * Template file for CprE 288 Lab 6
-// *
-// * @author Diane Rover, 2/15/2020
-// *
-// */
-//
 #include "open_interface.h"
+#include "cyBot_uart.h"
 #include "cyBot_Scan.h"
 #include <stdio.h>
 #include "movement.h"
@@ -16,58 +8,72 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include "uart-interrupt.h"
 
-#define REPLACEME 0
-
-void detectObject(void){
-
+void cyBot_sendString(char *input) {
+ while(*input != '\0') {
+        cyBot_sendByte(*input);
+        input++;
+    }
 }
-float avgDistance_IR(int i, cyBOT_Scan_t *scan_data) {
+float avgIR(int angle, cyBOT_Scan_t *scan_data) {
     float sum = 0;
-   while(i<=6){
-   cyBOT_Scan(i, scan_data);
-   sum += scan_data -> IR_raw_val;
-   i++;
-   }
-   return sum / 6;
+    int samples = 5;
+
+    for(int i = 0; i < samples; i++) {
+        cyBOT_Scan(angle, scan_data);
+        sum += scan_data->IR_raw_val;
+    }
+
+    return sum / samples;
 }
 
-float avgDistance_Ping(int i, cyBOT_Scan_t *scan_data) {
-    float sum = 0;
-   while(i<=6){
-   cyBOT_Scan(i, scan_data);
-   sum += scan_data -> sound_dist;
-   i++;
-   }
-   return sum / 6;
+void detectObject(cyBOT_Scan_t *scan) {
+    int angle;
+
+    int objStart = -1;
+    int objEnd = -1;
+    int objCount = 0;
+
+    float threshold = 50;  
+
+    char buffer[100];
+
+    for(angle = 0; angle <= 180; angle += 2) {
+
+        float ir = avgIR(angle, scan);
+
+        if(ir < threshold && objStart == -1) {
+            objStart = angle;
+        }
+
+        else if(ir >= threshold && objStart != -1) {
+            objEnd = angle;
+
+            int midAngle = (objStart + objEnd) / 2;
+
+            
+            cyBOT_Scan(midAngle, scan);
+            float distance = scan->sound_dist;
+
+        
+            float angleWidth = (objEnd - objStart) * (3.14159 / 180.0);
+
+            
+            float width = 2 * distance * tan(angleWidth / 2);
+
+            sprintf(buffer,
+                "Obj %d | Angle: %d | Dist: %.2f | Width: %.2f\n",
+                objCount, midAngle, distance, width);
+
+            cyBot_sendString(buffer);
+
+            objCount++;
+
+            objStart = -1;
+            objEnd = -1;
+        }
+    }
 }
-
-
-//int main(void) {
-//	timer_init();
-//	lcd_init();
-//	uart_interrupt_init();
-//
-//	command_byte = 'm';
-//
-//	uart_sendStr("UART ready\r\n");
-//	while(1)
-//	{
-//
-//	    if(command_flag == 1){
-//	        lcd_printf("M detected");
-//	        uart_sendStr(" receieved\r\n");
-//	        command_flag = 0;
-//	    }
-//
-//	}
-//
-//}
-//
-
-
-
 
 //int i;
 //bool objDetected = false;
@@ -100,25 +106,27 @@ float avgDistance_Ping(int i, cyBOT_Scan_t *scan_data) {
 int main(void) {
     timer_init();
     lcd_init();
+    cyBot_uart_init();
     cyBOT_init_Scan(0b0011);
     right_calibration_value = 122500;
      left_calibration_value = 1477000;
-    cyBOT_SERVO_cal();
-//     cyBOT_Scan_t *getScan = calloc(1, sizeof(cyBOT_Scan_t));
-//     detectObject(getScan);
-////     char buffer[100];
-////
-////    cyBOT_Scan_t *getScan = calloc(1, sizeof(cyBOT_Scan_t));
+//    cyBOT_SERVO_cal();
+     cyBOT_Scan_t *getScan = calloc(1, sizeof(cyBOT_Scan_t));
+     detectObject(getScan);
+//     char buffer[100];
 //
-////    char buffer[100];
-////    while(1){
-////        char test = (char) cyBot_getByte();
-////
-////    sprintf(buffer, "Got an %c", test);
-////    cyBot_sendString(buffer);
-////    if(cyBot_getByte() == 'p'){
-////        break;
-////    }
-////}
+//    cyBOT_Scan_t *getScan = calloc(1, sizeof(cyBOT_Scan_t));
+
+//    char buffer[100];
+//    while(1){
+//        char test = (char) cyBot_getByte();
+//
+//    sprintf(buffer, "Got an %c", test);
+//    cyBot_sendString(buffer);
+//    if(cyBot_getByte() == 'p'){
+//        break;
+//    }
+//}
     return 0;
 }
+
