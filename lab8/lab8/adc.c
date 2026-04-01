5c6f876738605fc101397cebac635cef4912e835
Jake Kunka <bmkunka@iastat.edu>
2026-04-01 14:13:41 -0500

    adc up

---
+++ lab8/lab8/adc.c
@@ -0,0 +1,40 @@
+#include "adc.h"
+#include "tm4c123gh6pm.h"
+
+void adc_init(void) {
+    SYSCTL_RCGCGPIO_R |= 0x02;  
+    SYSCTL_RCGCADC_R |= 0x01;    
+    while((SYSCTL_PRGPIO_R & 0x02) == 0) {};
+    while((SYSCTL_PRADC_R & 0x01) == 0) {};
+
+    GPIO_PORTB_DIR_R &= ~0x10;  
+    GPIO_PORTB_AFSEL_R |= 0x10;  
+    GPIO_PORTB_DEN_R &= ~0x10;   
+    GPIO_PORTB_AMSEL_R |= 0x10;  
+
+    ADC0_ACTSS_R &= ~0x01;
+
+    ADC0_EMUX_R &= ~0x000F;
+
+    ADC0_SSMUX0_R = 10;
+
+    ADC0_SSCTL0_R = 0x06;
+
+    ADC0_IM_R &= ~0x01;
+
+    ADC0_ACTSS_R |= 0x01;
+}
+
+uint16_t adc_read(void) {
+    uint16_t result;
+
+    ADC0_PSSI_R = 0x01;
+
+    while((ADC0_RIS_R & 0x01) == 0);
+
+    result = ADC0_SSFIFO0_R & 0x0FFF;
+
+    ADC0_ISC_R = 0x01;
+
+    return result;
+}
---
+++ lab8/lab8/adc.h
@@ -0,0 +1,9 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+void adc_init(void);
+
+uint16_t adc_read(void);
+
+#endif
