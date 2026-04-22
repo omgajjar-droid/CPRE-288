f27b1ea0ea3cbe0cf186ebf70cf14a1f77df0b82
Om Gajjar <omgajjar@iastate.edu>
2026-04-22 13:46:41 -0500

    Update

--- lab10/servo.c
+++ lab10/servo.c
@@ -4,66 +4,128 @@
  *  Created on: Apr 8, 2026
  *      Author: bmkunka
  */
-
 #include "Timer.h"
 #include "servo.h"
+#include "lcd.h"
+#include "button.h"
 
-volatile uint32_t setting_0;
-volatile uint32_t setting_180;
+#define COUNTS_0        16000
+#define COUNTS_180      32000
+#define COUNTS_PER_DEG  ((float)(COUNTS_180 - COUNTS_0) / 180.0f)
+
+volatile uint32_t setting_0 = COUNTS_0;
+volatile uint32_t setting_180 = COUNTS_180;
 volatile uint8_t calibration = 0;
 
 void servo_init(void){
-   SYSCTL_RCGCGPIO_R |= 0x02; //Port B
-   SYSCTL_RCGCTIMER_R |= 0x02; // Timer 1
-
-   while((SYSCTL_PRGPIO_R & 0x02) != 0x02){}; // Checking if Port B is ready
-   while((SYSCTL_PRTIMER_R & 0x02) != 0x02){}; // Checking if Timer 1 is ready
-
-  // this is for setting up T1CCP1 on PB5
-  GPIO_PORTB_AFSEL_R |= 0x20;
-  GPIO_PORTB_PCTL_R  &= ~0x00F00000;
-  GPIO_PORTB_PCTL_R |= 0x00700000;
-  GPIO_PORTB_DEN_R |= 0x20;
-  GPIO_PORTB_DIR_R |= 0x20;
-  
-  TIMER1_CTL_R &= ~0x100; // diable time
-  TIMER1_CFG_R = 0x4; // setting 16 bits
-  TIMER1_TBMR_R = 0x000A; // The PWC mode 
-
-  // Set period to 320000 counts, which should go to 20ms at 16MHz
-  uint32_t pwm_period = 320000;
-  TIMER1_TBILR_R &= 0x0;
-  TIMER1_TBILR_R = pwm_period & 0x0FFFF;
-  TIMER1_TBPR_R &= 0x0;
-  TIMER1_TBPR_R = pwm_period >> 16;
-  TIMER1_TBMATCHR_R &= 0x0;
-  TIMER1_TBPMR_R &= 0x0;
-
+    SYSCTL_RCGCTIMER_R |= 0x2; // enable port B timer
+    while ((SYSCTL_PRTIMER_R & 0x2) == 0) {} // wait for port B timer to be ready
+    TIMER1_CTL_R &= ~0x100; // diable time
+    TIMER1_CTL_R |= 0x4000; // invert pwm out signal so that match is low and rest of period is high
+    TIMER1_CFG_R |= 0x4; // setting 16 bits
+    TIMER1_TBMR_R |= 0b1010; // The PWC mode
+    TIMER1_TBPR_R  |= 0x4; // extends TBILR timer with extra prescaler register 3 bits
+    TIMER1_TBILR_R |= 0xE200; // Set period to 320000 counts, which should go to 20ms at 16MHz
+    TIMER1_TBPMR_R &= 0x00; // set prescale match register to 0x00 so no default to 0xFF
+    // initial position 90 deg
+    uint32_t start = COUNTS_0 + (uint32_t)(90 * COUNTS_PER_DEG);
+    TIMER1_TBMATCHR_R = start & 0xFFFF;
+    TIMER1_TBPMR_R    = (start >> 16) & 0xFF;
+    // init PB5
+    SYSCTL_RCGCGPIO_R |= 0x2;
+    while((SYSCTL_PRGPIO_R & 0x2) == 0) {}
+    GPIO_PORTB_AFSEL_R |= 0x20;
+    GPIO_PORTB_PCTL_R  |= 0x700000;
+    GPIO_PORTB_DEN_R   |= 0x20;
+    GPIO_PORTB_DIR_R   |= 0x20;
     TIMER1_CTL_R |= 0x100; // Enable time
 }
-// rest get the servo to move 
+
+// rest get the servo to move
 void servo_move(uint16_t degrees){
-  long match_period;
-  long match_shifted;
-   
+    if(degrees > 180) degrees = 180;
+    uint32_t match;
     if(!calibration){
-        float match_value_ms = 20.0 - (degrees/180.0+1.0);
-        match_period = (match_value_ms / 1000)/0.0000000625;
+        match = COUNTS_0 + (uint32_t)(degrees * COUNTS_PER_DEG);
     }
     else{
-        match_period = (((signed int)(setting_180 - setting_0))/180) * degrees + setting_0;
+        match = setting_0 + (uint32_t)(degrees * ((float)(setting_180 - setting_0) / 180.0f));
     }
-   
-    match_shifted = match_period >> 16;
-
-    TIMER1_TBMATCHR_R = match_period & 0x0FFFF;
-    TIMER1_TBPMR_R = match_shifted; 
+    TIMER1_TBMATCHR_R = match & 0xFFFF;
+    TIMER1_TBPMR_R    = (match >> 16) & 0xFF;
+    timer_waitMillis(500);
 }
 
 // calibrate the servo
 void servo_calibration(void){
+    char buffer[90];
+    uint8_t button = 0;
 
-   
+    // Step 1: set 0 degrees
+    servo_move(0);
+    lcd_printf("Nudge to 0 deg\nB4 to confirm");
+    timer_waitMillis(1000);
+
+    while(button != 4){
+        uint32_t match = ((TIMER1_TBPMR_R & 0xFF) << 16)
+                        | (TIMER1_TBMATCHR_R & 0xFFFF);
+        button = button_getButton();
+        switch(button){
+            case 1:
+                match += 10;
+                timer_waitMillis(200);
+                break;
+            case 2:
+                match -= 10;
+                timer_waitMillis(200);
+                break;
+            case 3:
+                match += 100;
+                timer_waitMillis(200);
+                break;
+        }
+        TIMER1_TBMATCHR_R = match & 0xFFFF;
+        TIMER1_TBPMR_R    = (match >> 16) & 0xFF;
+        sprintf(buffer, "%u | Set 0", match);
+        lcd_printf(buffer);
+    }
+    setting_0 = ((TIMER1_TBPMR_R & 0xFF) << 16) | (TIMER1_TBMATCHR_R & 0xFFFF);
+    lcd_printf("0 deg locked in!");
+    timer_waitMillis(1000);
+
+    // Step 2: set 180 degrees
+    servo_move(180);
+    lcd_printf("Nudge to 180 deg\nB4 to confirm");
+    timer_waitMillis(1000);
+
+    button = 0;
+    while(button != 4){
+        uint32_t match = ((TIMER1_TBPMR_R & 0xFF) << 16)
+                        | (TIMER1_TBMATCHR_R & 0xFFFF);
+        button = button_getButton();
+        switch(button){
+            case 1:
+                match += 10;
+                timer_waitMillis(200);
+                break;
+            case 2:
+                match -= 10;
+                timer_waitMillis(200);
+                break;
+            case 3:
+                match += 100;
+                timer_waitMillis(200);
+                break;
+        }
+        TIMER1_TBMATCHR_R = match & 0xFFFF;
+        TIMER1_TBPMR_R    = (match >> 16) & 0xFF;
+        sprintf(buffer, "%u | Set 180", match);
+        lcd_printf(buffer);
+    }
+    setting_180 = ((TIMER1_TBPMR_R & 0xFF) << 16) | (TIMER1_TBMATCHR_R & 0xFFFF);
+    lcd_printf("180 deg locked!");
+    timer_waitMillis(1000);
+
+    calibration = 1;
+    servo_move(90);
 }
-  
-
